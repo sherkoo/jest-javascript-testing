4f09d6483485fbc0af114a7937482087336c0dfa
s <stephenherko@gmail.com>
2022-10-08 12:34:42 -0500

    Update async.test.js

--- tests/async.test.js
+++ tests/async.test.js
@@ -1,8 +1,16 @@
 const {makeFunction} = require('../scripts/async.js');
 
 describe('Async function test', () => {
-  test('the data is peanut butter', async () => {
-    const data = await makeFunction(1);
-    expect(data).toBe('it worked!');
+  describe('first async test', () => {
+    test('should equal it worked', async () => {
+      const data = await makeFunction(1);
+      expect(data).toBe('it worked!');
+    });
+
+    test('this test should fail', async () => {
+      const data = await makeFunction(1);
+      expect(data).toBe('it worked!1');
+    });
   });
-})
+
+});
